37ac467c3f85b006f94b7fd463cbd007e602f10e
yuka-lamp <oonishi@lamp.jp>
2020-12-11 17:38:11 +0900

    front

--- front-page.php
+++ front-page.php
@@ -2,13 +2,89 @@
 $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); ?>
-<div class="position-relative overflow-hidden text-center bg-light">
-<div class="col-md-5 p-lg-5 mx-auto my-5">
-<h1 class="display-4 font-weight-normal">Punny headline</h1>
-<p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-<a class="btn btn-outline-secondary" href="#">Coming soon</a>
-</div>
-<div class="product-device shadow-sm d-none d-md-block"></div>
-<div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
-</div>
+
+<section class="top__about py-5">
+  <div class="container">
+  <h2 class="top__about-ttl">「ものづくり」で地域社会へ貢献を</h2>
+  <p class="text-secondary font-weight-bold">Contributing to the local community<br>through “manufacturing”</p>
+  <p class="w-75">当社「ファーマーズ・フォレスト」は、ろまんちっく村をはじめとする交流資源を通じ、次世代のアグリカルチャーとエコツーリズムを目指した「ものづくり」「ひとづくり」「まちづくり」に挑戦し、全国の各地域と連携協働しながら、活力ある「ローカルブランド」を総合的にプロデュースしてまいりたい考えております。<br>当社「ファーマーズ・フォレスト」は、ろまんちっく村をはじめとする交流</p>
+  </div>
+</section>
+
+<section class="top__links py-5">
+  <div class="row bg-primary">
+    <div class="top__links-item col-sm-6 py-5">
+      <a class="d-block text-center" href="#">
+        <h2 class="ttl2 text-white">Service</h2>
+        <p class="text-white mt-3">事業紹介</p>
+        <div class="mt-4 text-center">
+        <a class="btn btn-primary" href="<?php echo $home; ?>/service/">詳しくはこちら</a>
+        </div>
+      </a>
+    </div>
+    <div class="top__links-item col-sm-6 py-5">
+      <a class="d-block text-center" href="#">
+        <h2 class="ttl2 text-white">Company</h2>
+        <p class="text-white mt-3">会社概要</p>
+        <div class="mt-4 text-center">
+        <a class="btn btn-primary" href="<?php echo $home; ?>/company/">詳しくはこちら</a>
+        </div>
+      </a>
+    </div>
+    <div class="top__links-item col-sm-6 py-5">
+      <a class="d-block text-center" href="#">
+        <h2 class="ttl2 text-white">Tour</h2>
+        <p class="text-white mt-3">ツアー販売</p>
+        <div class="mt-4 text-center">
+        <a class="btn btn-primary" href="<?php echo $home; ?>/tour/">詳しくはこちら</a>
+        </div>
+      </a>
+    </div>
+    <div class="top__links-item col-sm-6 py-5">
+      <a class="d-block text-center" href="#">
+        <h2 class="ttl2 text-white">Product</h2>
+        <p class="text-white mt-3">認証商品</p>
+        <div class="mt-4 text-center">
+        <a class="btn btn-primary" href="<?php echo $home; ?>/product/">詳しくはこちら</a>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="container mt-5">
+    <div class="bg-primary">
+      <div class="top__links-item py-5">
+        <a class="d-block text-center" href="#">
+          <h2 class="ttl2 text-white">product</h2>
+          <p class="text-white mt-3">認証商品</p>
+          <div class="mt-4 text-center">
+          <a class="btn btn-primary" href="<?php echo $home; ?>/product/">詳しくはこちら</a>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="top__blog blog">
+  <div class="container">
+    <div class="blog__wrap row">
+      <div class="blog__item col-sm-4">
+        <div class="blog__item__img">
+          <a class="d-block" hraf="#">
+            <img class="bg-secondary" src="#" alt="">
+          </a>
+        </div>
+        <div class="blog__item__txt">
+          <a class="d-block" hraf="#">
+            <p class="text-weight-bold">地域の隠れた資源やストーリーを掘り起こして組み合わせ地域の新たな魅力</p>
+            <p class="blog__item__txt-tag text-gray">FOOD</p>
+            <p class="blog__item__txt-date text-gray">2020.01.01</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 <?php get_footer();
--- header.php
+++ header.php
@@ -11,22 +11,77 @@ $wp_url = get_template_directory_uri(); ?>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<header>
-<nav class="site-header sticky-top py-1 bg-dark">
-<div class="container d-flex flex-column flex-md-row">
-<a class="py-2 text-white" href="#" aria-label="Product">LOGO</a>
-<div class="ml-md-auto">
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Tour</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Product</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Features</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Enterprise</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Support</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Pricing</a>
-<a class="p-2 d-none d-md-inline-block text-white" href="#">Cart</a>
-</div>
-</div>
-</nav>
+<header class="header">
+  <nav class="header__wrap navbar navbar-light bg-light">
+    <h1 class="header__wrap__logo navbar-brand" href="<?php echo $home ?>">
+      <img src="<?php echo $wp_url ?>/dist/images/logo.svg" alt="五條市地域商社のロゴ">
+    </h1>
+    <button class="header__wrap__hum navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="header__nav collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="header__nav-inner navbar-nav">
+        <li class="header__nav-item nav-item">
+          <a class="header__nav-link nav-link text-uppercase text-body" href="<?php echo $home ?>">top</a>
+        </li>
+        <li class="header__nav-item nav-item">
+          <a class="header__nav-link nav-link text-uppercase text-primary" href="<?php echo $home ?>/concept/">concept</a>
+        </li>
+        <li class="header__nav-item nav-item">
+          <a class="header__nav-link nav-link text-uppercase text-primary" href="<?php echo $home ?>/service/">service</a>
+        </li>
+        <li class="header__nav-item nav-item">
+          <a class="header__nav-link nav-link text-uppercase text-primary" href="<?php echo $home ?>/company/">company</a>
+        </li>
+        <li class="header__nav-item nav-item">
+          <a class="header__nav-link nav-link text-uppercase text-primary" href="<?php echo $home ?>/tour/">tour</a>
+        </li>
+        <li class="header__nav-item nav-item">
+          <a class="header__nav-link nav-link text-uppercase text-primary" href="<?php echo $home ?>/blog/">blog</a>
+        </li>
+        <li class="header__nav-link nav-item">
+          <ul class="list-unstyled">
+            <li class="header__nav__sub-item nav-item">
+              <a class="header__nav__sub-link nav-link small text-gray" href="<?php echo $home ?>/contact/">お問い合わせ</a>
+            </li>
+            <li class="header__nav-item nav-item">
+              <a class="header__nav__sub-link nav-link small text-gray" href="<?php echo $home ?>/tarm/">利用規約</a>
+            </li>
+            <li class="header__nav-item nav-item">
+              <a class="header__nav__sub-link nav-link small text-gray" href="<?php echo $home ?>/privacy-policy">プライバシーポリシー</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </header>
 <!-- /header -->
 
 <main>
+
+<?php if (is_front_page()): ?>
+
+<section class="mv">
+  <div class="mv__content">
+    <img src="<?php echo $wp_url ?>/dist/images/mv_logo.png" alt="五條市地域商社のロゴ" srcset="<?php echo $wp_url ?>/dist/images/mv_logo.png 1x, <?php echo $wp_url ?>/dist/images/mv_logo@2x.png 2x">
+    <p class="mv__content-ttl text-uppercase font-weight-bold">FARMERS<br>FOREST<br>GROUP</p>
+  </div>
+</section>
+
+<section class="info bg-success py-4">
+  <div class="row">
+    <div class="col-sm-4">
+      <p class="text-uppercase font-weight-bold m-0">Information</p>
+    </div>
+    <a class="col-sm-8 text-secondary text-decoladion_underline" href="#"><span class="mr-5">2020.01.01</span>お知らせの内容が入ります。</a>
+  </div>
+</section>
+
+<?php else:?>
+
+<section class="submv">
+
+</section>
+
+<?php endif; ?>
