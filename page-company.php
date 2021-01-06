60887796265129392efa17567d13e0c54e38027d
yuka-lamp <oonishi@lamp.jp>
2021-01-06 15:01:07 +0900

    campany

--- page-company.php
+++ page-company.php
@@ -3,11 +3,145 @@ $home = esc_url(home_url());
 $wp_url = get_template_directory_uri();
 get_header(); the_post(); ?>
 
-<!-- ▼ 導入 -->
-<section class="">
+<!-- ▼ メッセージ -->
+<section class="company__message">
   <div class="container">
+    <div class="main-ttl mb-5">
+      <h2 class="text-secondary font-weight-bold">message<span class="text-primary ml-4">代表からのメッセージ</span></h2>
+    </div>
+    <h2 class="mt-5 mb-5">地域を繋ぐ<br>プラットフォームに</h2>
+    <p class="d-block text-left lh-l cc">五條市地域商社株式会社は、五條市内に点在する魅力や課題を整理し、有機的に繋ぎ合わせるプラットフォームとして官民連携で地域に新たな魅力と価値を創造します。<br>五條市にある生産物、観光、人材などの魅力的な資源全てを取り扱い、商社としてPRし活性化を行うことで、稼げるまちづくりを実現していきます。五條市地域商社株式会社は、五條市内に点在する魅力や課題を整理し、有機的に繋ぎ合わせるプラットフォームとして官民連携で地域に新たな魅力と価値を創造します。<br>五條市にある生産物、観光、人材などの魅力的な資源全てを取り扱い、商社としてPRし活性化を行うことで、稼げるまちづくりを実現していきます。</p>
+    <!-- ▼ 画像 -->
+    <div class="d-block d-md-flex mt-5">
+      <div class="col-12 col-md-4 m-0 p-0">
+        <img class="w-100" src="<?php echo $wp_url ?>/dist/images/company_msg_1.png" alt="五條市地域商社の写真" srcset="<?php echo $wp_url ?>/dist/images/company_msg_1.png 1x, <?php echo $wp_url ?>/dist/images/company_msg_1@2x.png 2x">
+      </div>
+      <div class="col-4 d-none d-md-block m-0 p-0">
+        <img class="w-100" src="<?php echo $wp_url ?>/dist/images/company_msg_2.png" alt="五條市地域商社の写真" srcset="<?php echo $wp_url ?>/dist/images/company_msg_2.png 1x, <?php echo $wp_url ?>/dist/images/company_msg_2@2x.png 2x">
+      </div>
+      <div class="col-4 d-none d-md-block m-0 p-0">
+        <img class="w-100" src="<?php echo $wp_url ?>/dist/images/company_msg_3.png" alt="五條市地域商社の写真" srcset="<?php echo $wp_url ?>/dist/images/company_msg_3.png 1x, <?php echo $wp_url ?>/dist/images/company_msg_3@2x.png 2x">
+      </div>
+    </div>
+    <!-- ▲ 画像 -->
   </div>
 </section>
-<!-- ▲ 導入 -->
+<!-- ▲ メッセージ -->
+
+<!-- ▼ 会社情報 -->
+<section class="company__profile sec-t">
+  <div class="container">
+    <div class="main-ttl mb-5">
+      <h2 class="text-secondary font-weight-bold">profile<span class="text-primary ml-4">会社情報</span></h2>
+    </div>
+    <!-- ▼ テーブル -->
+    <table class="row">
+      <tbody class="col-12 col-md-6">
+        <tr>
+          <th class="font-weight-bold">会社名</th>
+          <td>五條市地域商社 株式会社</td>
+        </tr>
+        <tr>
+          <th class="font-weight-bold">所在地</th>
+          <td>
+            〒604-0024<br>京都府京都市中京区下妙覚寺町195 KMGビル4F
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104994.9491861849!2d135.41602455284243!3d34.67762336975711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e6553406e2e1%3A0xc55bc16ee46a2fe7!2z5aSn6Ziq5bqc5aSn6Ziq5biC!5e0!3m2!1sja!2sjp!4v1609902105587!5m2!1sja!2sjp" width="100%" height="137px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          </td>
+        </tr>
+        <tr>
+          <th class="font-weight-bold">TEL</th>
+          <td>五條市地域商社 株式会社</td>
+        </tr>
+      </tbody>
+      <tbody class="col-12 col-md-6">
+        <tr>
+          <th class="font-weight-bold">代表者名</th>
+          <td>五條市地域商社 株式会社</td>
+        </tr>
+        <tr>
+          <th class="font-weight-bold">設立</th>
+          <td>〒604-0024<br>京都府京都市中京区下妙覚寺町195 KMGビル4F</td>
+        </tr>
+        <tr>
+          <th class="font-weight-bold">資本金</th>
+          <td>五條市地域商社 株式会社</td>
+        </tr>
+        <tr>
+          <th class="font-weight-bold">従業員</th>
+          <td>五條市地域商社 株式会社</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- ▲ テーブル -->
+  </div>
+</section>
+<!-- ▲ 会社情報 -->
+
+<!-- ▼ 認定マークについて -->
+<section class="company__mark sec-t">
+  <div class="container">
+    <div class="main-ttl mb-5">
+      <h2 class="text-secondary font-weight-bold">mark<span class="text-primary ml-4">認定マークについて</span></h2>
+    </div>
+    <!-- ▼ コンテンツ -->
+    <div class="company__mark-content d-flex flex-wrap justify-content-between mt-5">
+      <div class="company__mark-text">
+        <h2 class="mt-3 mb-4">地域を繋ぐプラットフォームに</h2>
+        <p class="d-block text-left lh-l m-0">五條市地域商社株式会社は、五條市内に点在する魅力や課題を整理し、有機的に繋ぎ合わせるプラットフォームとして官民連携で地域に新たな魅力と価値を創造します。<br>五條市にある生産物、観光、人材などの魅力的な資源全てを取り扱い、商社としてPRし活性化を行うことで、</p>
+      </div>
+      <div class="company__mark-img">
+        <img src="<?php echo $wp_url ?>/dist/images/company_mark.png" alt="" srcset="<?php echo $wp_url ?>/dist/images/company_mark.png 1x, <?php echo $wp_url ?>/dist/images/company_mark@2x.png 2x">
+      </div>
+    </div>
+    <!-- ▲ コンテンツ -->
+  </div>
+</section>
+<!-- ▲ 認定マークについて -->
+
+<!-- ▼ メンバー紹介 -->
+<section class="company__member sec-t">
+  <div class="container">
+    <div class="main-ttl mb-4">
+      <h2 class="text-secondary font-weight-bold">member<span class="text-primary ml-4">メンバー紹介</span></h2>
+    </div>
+    <p class="d-block text-left lh-l pb-4">五條市地域商社株式会社は、五條市内に点在する魅力や課題を整理し、有機的に繋ぎ合わせるプラットフォームとして官民連携で地域に新たな魅力と価値を創造します。<br>五條市にある生産物、観光、人材などの魅力的な資源全てを取り扱い、商社としてPRし活性化を行うことで、</p>
+    <!-- ▼ 一覧 -->
+    <div class="d-flex flex-wrap">
+      <?php query_posts( array(
+        'post_type' => 'member', //カスタム投稿名を指定
+        'posts_per_page' => -1      ///表示件数（-1で全ての記事を表示）
+      ));
+      $id = get_the_ID();
+      ?>
+      <?php if(have_posts()): ?>
+      <?php while(have_posts()):the_post(); ?>
+      <!-- ▼ ループするコンテンツ -->
+      <div class="company__member__item">
+        <!-- ▼ 写真 -->
+        <div class="company__member__item-img mb-3">
+          <?php if( get_field('photo')):  // 画像がある場合 ?>
+            <img class="w-100" src="<?php echo the_field('photo'); ?>" alt="<?php the_title(); ?>">
+          <?php else: // 画像がない場合 ?>
+            <img class="w-100" src="<?php echo $wp_url ?>/dist/images/company_member_imgnone.png" alt="<?php the_title(); ?>" srcset="<?php echo $wp_url ?>/dist/images/company_member_imgnone.png 1x, <?php echo $wp_url ?>/dist/images/company_member_imgnone@2x.png 2x">
+          <?php endif; ?>
+        </div>
+        <!-- ▲ 写真 -->
+        <!-- ▼ テキスト -->
+        <div class="company__member__item-txt">
+          <p class="d-block  f-12 mb-1"><?php echo the_field('position'); ?></p>
+          <h3 class="d-block f-15 font-weight-bold mb-1"><?php the_title(); ?></h3>
+          <p class="company__member__item-en d-block f-12 text-uppercase font-weight-bold text-info"><?php echo the_field('name-en'); ?></p>
+        </div>
+        <!-- ▲ テキスト -->
+      </div>
+      <!-- ▲ ループするコンテンツ -->
+      <?php endwhile; else: ?>
+      <?php endif; ?>
+      <?php wp_reset_query(); ?>
+    </div>
+    <!-- ▲ 一覧 -->
+  </div>
+</section>
+<!-- ▲ メンバー紹介 -->
 
 <?php get_footer();
